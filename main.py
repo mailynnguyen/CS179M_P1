8a3a0169aa0d236477ab15cfeedc91fed82fe94a
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2025-10-23 16:42:02 -0700

    nearest neighbor swap anytime

--- main.py
+++ main.py
@@ -1,28 +1,9 @@
 import math
 from numpy import random
 import numpy as np
-
-# data_file = open("128Circle201.txt", "r")
-
-# # starting_point = data_file.readline().strip()
-# # print(starting_point)
-
-# coordinates = []
-# for line in data_file:
-#     coordinates.append([float(c) for c in line.strip().split()]) # ['number', 'number']
-
-# def calc_route_distance(route, start_city, euclidean_dist):
-#     total_distance = 0
-#     curr_city = start_city
-#     for next_city in route:
-#         total_distance += euclidean_dist(curr_city, next_city)
-#         curr_city = next_city
-#     total_distance += euclidean_dist(curr_city, start_city)
-#     return total_distance
     
 def euclidean_distance(c1, c2):
     return math.sqrt((c1[0] - c2[0])**2 + (c1[1] - c2[1])**2)
-
 
 
 def create_distance_matrix(filename, num_nodes):
@@ -30,20 +11,6 @@ def create_distance_matrix(filename, num_nodes):
     coordinates = []
     for line in data_file:
         coordinates.append([float(c) for c in line.strip().split()]) # ['number', 'number']
-    # print(coordinates)
-
-    
-    
-    # i = 0
-    # shortest_distance = float('inf')
-    # for route in possible_routes:
-    #     print(i)
-    #     route_distance = calc_route_distance(route, coordinates[0], euclidean_distance)
-    #     if route_distance < shortest_distance:
-    #         shortest_distance = route_distance
-    #     i += 1
-
-    # print(shortest_distance)
 
     distance_matrix = [[0] * num_nodes for _ in range(num_nodes)]
     for x in range(num_nodes):
@@ -52,6 +19,42 @@ def create_distance_matrix(filename, num_nodes):
     return distance_matrix
     # print(distance_matrix)
 
+
+def calc_nearest_neighbor(distance_matrix, num_nodes):
+    total_distance = 0
+    visited = [0] # array to hold all the visited nodes
+    i = 0 # i = current node/point
+    while len(visited) != num_nodes: # looping through all indexes/rows/points
+        nearest_neighbor_distance = float('inf') # current nearest neighbor distance is inf
+        for j in range(num_nodes): # looping thorugh all points/columns
+
+            if i != j and j not in visited: # if the point is not the same as the curr point and has not been visited yet
+                neighbor_distance = distance_matrix[i][j] # check the distance from the curr point to the neighboring point
+
+                if neighbor_distance < nearest_neighbor_distance: # if the distance from curr point to neighboring point is less than the smallest neighboring distance
+                    nearest_neighbor_distance = neighbor_distance # assign that to be the nearest neighbor distance
+                    nearest_neighbor = j # make the neighboring point to be the designated nearest neighbor
+
+        total_distance += nearest_neighbor_distance # add up the total distance using the shortest nearest neighbor position
+        visited.append(nearest_neighbor) # add that point to visited 
+        i = nearest_neighbor # make the curr point the nearest neighbor point
+
+    total_distance += distance_matrix[i][0] # add up the distance from the ending point to the starting point
+
+    # print(total_distance)
+
+    return visited, total_distance
+
+
+def calc_nearest_neighbor_with_solution(distance_matrix, curr_solution):
+    total_distance = 0
+    curr_node = curr_solution[0] # set the curr node to be the first node in the solution, which is the starting node, should be 0
+    for next_node in curr_solution: # loop through all the nodes in the solution 
+        total_distance += distance_matrix[curr_node][next_node] # find the distance btw the curr node and the next node and add to total distance
+        curr_node = next_node # set the curr node to be the next node (the next node will move forward)
+    total_distance += distance_matrix[curr_node][0] # add the distance of the last node to the first to the total distance
+
+    return total_distance
 
 
 def main():
@@ -62,8 +65,9 @@ def main():
     for _ in data_file:
         num_nodes += 1
     print(f"There are {num_nodes} nodes, computing route..")
+    print("\tShortest Route Discovered So Far")
 
-    distance_matrix = create_distance_matrix(filename, num_nodes)
+    distance_matrix = create_distance_matrix(filename, num_nodes) # creates a distance matrix that holds the distance between node x and y at distance_matrix[x][y]
 
     # RANDOM SEARCH
     # shortest_distance = float('inf')
@@ -93,29 +97,28 @@ def main():
     #     i += 1
 
     # NEAREST NEIGHBOR
-    shortest_distance = float('inf')
+    shortest_solution, shortest_distance = calc_nearest_neighbor(distance_matrix, num_nodes)
+    print(f"\t\t{shortest_distance}")
 
-    total_distance = 0
-    visited = [0] # array to hold all the visited nodes
-    i = 0 # i = current node/point
-    while len(visited) != 128: # looping through all indexes/rows/points
-        nearest_neighbor_distance = float('inf') # current nearest neighbor distance is inf
-        for j in range(num_nodes): # looping thorugh all points/columns
+    while True:
+        solution = shortest_solution # copy the shortest solutiont to use
 
-            if i != j and j not in visited: # if the point is not the same as the curr point and has not been visited yet
-                neighbor_distance = distance_matrix[i][j] # check the distance from the curr point to the neighboring point
+        node1 = random.randint(1, num_nodes) # get a randon first node, doesn't choose the starting node
+        if node1 < num_nodes - 3: # 
+            node2 = node1 + 3
+        else:
+            node2 = num_nodes - random.randint(1, 3)
 
-                if neighbor_distance < nearest_neighbor_distance: # if the distance from curr point to neighboring point is less than the smallest neighboring distance
-                    nearest_neighbor_distance = neighbor_distance # assign that to be the nearest neighbor distance
-                    nearest_neighbor = j # make the neighboring point to be the designated nearest neighbor
+        solution[node1], solution[node2] = solution[node2], solution[node1] # swap the two nodes
 
-        total_distance += nearest_neighbor_distance # add up the total distance using the shortest nearest neighbor position
-        visited.append(nearest_neighbor) # add that point to visited 
-        i = nearest_neighbor # make the curr point the nearest neighbor point
+        total_distance = calc_nearest_neighbor_with_solution(distance_matrix, solution) # calcs the nearest neighbor with the swapped solution and returns the total distance
 
-    total_distance += distance_matrix[i][0] # add up the distance from the ending point to the starting point
+        if total_distance < shortest_distance:
+            shortest_distance = total_distance
+            shortest_solution = solution
+            print(shortest_distance)
 
-    print(total_distance)
+
         
 
 
